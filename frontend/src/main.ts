fc8c05d05a0fcda2d435519b151e8d812bef1f41
muje hoxe <mujehoxe@gmail.com>
2024-03-20 07:56:18 +0100

    notification displayed in main.ts instead of index.html

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -10,10 +10,16 @@ document.querySelector("#app")!.innerHTML = `
     </div>
     <div id="pdf-display">
     </div>
+    <div
+      id="notification"
+      aria-live="assertive"
+      class="pointer-events-none fixed left-0 inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-50"
+    ></div>
   </div>
 `;
 
 renderForm();
+
 renderCvsPreviewSlideOver();
 declare global {
   interface Window {
