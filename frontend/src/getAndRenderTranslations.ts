37dd7f0b3a81278d950e621e8f78088f0996cab0
muje hoxe <mujehoxe@gmail.com>
2024-04-07 20:22:23 +0100

    refactor renderTranslations

--- frontend/src/getAndRenderTranslations.ts
+++ frontend/src/getAndRenderTranslations.ts
@@ -32,47 +32,50 @@ export function getAndRenderTranslations(e: Event) {
             )
           );
           aboutOtherLang.appendChild(aboutBox);
-          const doneEditing = document.createElement("div");
-          const editBtn = createEditBtn(aboutOtherLang, doneEditing, aboutBox);
-          createDoneEditingBtn(doneEditing, aboutOtherLang, editBtn);
+          const editBtn = createEditButton();
+          editBtn.onclick = () =>
+            handleToggleEditing(editBtn, doneEditing, aboutBox);
+          const doneEditing = createDoneEditingButton();
+          doneEditing.onclick = () =>
+            handleToggleEditing(editBtn, doneEditing, aboutBox);
           aboutOtherLang.appendChild(doneEditing);
+          aboutOtherLang.appendChild(editBtn);
           aboutOtherLangs!.appendChild(aboutOtherLang);
         })
         .catch((error) => console.error("Error translating text:", error));
   });
 }
 
-function createDoneEditingBtn(
-  doneEditing: HTMLDivElement,
-  editBtn: HTMLDivElement,
-  aboutBox: HTMLDivElement
-) {
-  doneEditing.classList.add("cursor-pointer", "hidden");
-  doneEditing.innerHTML = `Done <i class="fa-solid fa-check text-center text-xs"></i>`;
-  doneEditing.onclick = () => {
-    doneEditing.classList.toggle("hidden");
-    editBtn.classList.toggle("hidden");
-    aboutBox.contentEditable = "false";
-    aboutBox.classList.toggle("text-white/90");
-  };
+function createEditButton() {
+  const edit = document.createElement("div");
+  edit.classList.add(
+    ..."cursor-pointer inline p-1 text-xs rounded-md ring-1 rounded-5 ring-inset ring-white/20".split(
+      " "
+    )
+  );
+  edit.innerHTML = `Edit <i class="pl-1 mb-2 fa-solid fa-pen text-center text-xs"></i>`;
+  return edit;
+}
+function createDoneEditingButton() {
+  const doneEditing = document.createElement("div");
+  doneEditing.classList.add(
+    ..."cursor-pointer inline p-1 text-xs rounded-md ring-1 rounded-5 ring-inset ring-white/20 hidden".split(
+      " "
+    )
+  );
+  doneEditing.innerHTML = `Done <i class="pl-1 mb-2 fa-solid fa-check text-center text-xs"></i>`;
+  return doneEditing;
 }
 
-function createEditBtn(
-  aboutOtherLang: HTMLDivElement,
+function handleToggleEditing(
+  editBtn: HTMLDivElement,
   doneEditing: HTMLDivElement,
   aboutBox: HTMLDivElement
 ) {
-  const editBtn = document.createElement("div");
-  editBtn.classList.add("cursor-pointer");
-  editBtn.innerHTML = `Edit <i class="fa-solid fa-pen text-center text-xs"></i>`;
-  aboutOtherLang.appendChild(editBtn);
-  editBtn.onclick = () => {
-    editBtn.classList.toggle("hidden");
-    doneEditing.classList.toggle("hidden");
-    aboutBox.contentEditable = "true";
-    aboutBox.classList.toggle("text-white/90");
-  };
-  return editBtn;
+  editBtn.classList.toggle("hidden");
+  doneEditing.classList.toggle("hidden");
+  aboutBox.contentEditable = String(!aboutBox.isContentEditable);
+  aboutBox.classList.toggle("text-white/90");
 }
 
 export function areLanguagesEqual(lang1: Language, lang2: Language) {
