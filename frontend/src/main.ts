54e054daa7a3b4abeda7202cb755f52e9b32d6a4
muje hoxe <mujehoxe@gmail.com>
2024-05-09 07:43:52 +0100

    main.ts: pdf-display not needed, fix utils

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,18 +2,13 @@ import "./index.css";
 
 import { renderUserInfoForm } from "./components/form";
 import { renderCvsPreviewSlideOver } from "./components/cvs-preview";
-import { renderPdfDisplay } from "./components/pdf-display";
-import { renderFormLanguages } from "./components/languages";
+import { renderFormLanguages } from "./utils/languages";
 
 document.querySelector("#app")!.innerHTML = `
   <div id="root" class='h-screen'>
     <div class="px-12 pt-4" id="form-languages-containter"></div>
-    <div class="px-12 py-4" id="info-form-container">
-    </div>
-    <div id="cvs-preview" class="hidden">
-    </div>
-    <div id="pdf-display" class="hidden">
-    </div>
+    <div class="px-12 py-4" id="info-form-container"></div>
+    <div id="cvs-preview" class="hidden"></div>
     <div
       id="notification"
       aria-live="assertive"
@@ -27,5 +22,3 @@ renderFormLanguages();
 renderUserInfoForm();
 
 renderCvsPreviewSlideOver();
-
-renderPdfDisplay();
