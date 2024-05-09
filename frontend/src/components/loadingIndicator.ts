0ca925089ee6a9a8b204ac21830b8d2c8685dc41
muje hoxe <mujehoxe@gmail.com>
2024-05-09 07:26:01 +0100

    loading indicatior not specific to translations

--- frontend/src/components/loadingIndicator.ts
+++ frontend/src/components/loadingIndicator.ts
@@ -2,13 +2,13 @@ export function renderLoadingIndicator(parent: HTMLElement) {
   parent.innerHTML = `
 	<div id='loading' class="flex justify-start items-center min-h-12 p-2 rounded-md">
       <div class="animate-spin rounded-[50%] border-t-[#3498db] border-white border-[2px] border-solid w-4 h-4"></div>
-      <p class="text-white ml-2 text-sm font-medium">Loading translations...</p>
+      <p class="text-white ml-2 text-sm font-medium">Loading...</p>
   </div>
 	`;
 }
 
 export function removeLoadingIndicator(parent: HTMLElement) {
-  const loadingIndicator = parent.querySelector('#loading');
+  const loadingIndicator = parent.querySelector("#loading");
   if (loadingIndicator) {
     loadingIndicator.remove();
   }
